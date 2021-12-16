8fc09a370c4c9d5eca2d179407234837f97e6e2d
vange <vanger2607@gmail.com>
2021-12-16 16:14:28 +0300

    first steps

--- enemies.py
+++ enemies.py
@@ -1,6 +1,10 @@
+import pygame as pg
+
+
 class Enemy:
+    """общий класс для всех врагов"""
     def __init__(self, img, speed, hp, attack):
-        self.icon = img
+        self.icon = pg.image.load(img)
         self.speed = speed
         self.hp = hp
         self.attack = attack
