8d6be2fc945c05e39601b11b5cd6e0a3175ae3c8
vange <vanger2607@gmail.com>
2021-12-19 17:01:35 +0300

    fixed

--- SIM_Game/main.py
+++ SIM_Game/main.py
@@ -1,28 +1,36 @@
 import pygame
-import button
 
 pygame.init()
+
+
+class BTN(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super().__init__()
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
 
 width = 600
 height = 480
 size = (width, height)
 screen = pygame.display.set_mode(size)
 img = pygame.image.load('SIM.png')
-start_img = pygame.image.load('start.png').convert_alpha()
-exit_img = pygame.image.load('exit.png').convert_alpha()
-start_button = button.BTN(250, 200, start_img, 1)
-exit_button = button.BTN(250, 300, exit_img, 1)
+start_button = BTN(250, 200, 'start.png')
+exit_button = BTN(250, 300, 'exit.png')
 pygame.display.set_caption('SIM')
 running = True
-
+screen.blit(img, (0, 0))
+screen.blit(start_button.image, start_button.rect)
+screen.blit(exit_button.image, exit_button.rect)
 while running:
-    if start_button.draw(screen):
-        print('START')
-    if exit_button.draw(screen):
-        print('EXIT')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        screen.blit(img, (0, 0))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if start_button.rect.collidepoint(event.pos):
+                print('START')
+            elif exit_button.rect.collidepoint(event.pos):
+                print('EXIT')
     pygame.display.update()
 pygame.quit()
