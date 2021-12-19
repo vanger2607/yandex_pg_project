2d8625e465b09bac0fc46bcb2010afd161a032d0
Saveliy125 <Saveliy.korovin@yandex.ru>
2021-12-19 16:07:28 +0300

    BTN class

--- SIM_Game/main.py
+++ SIM_Game/main.py
@@ -10,8 +10,8 @@ screen = pygame.display.set_mode(size)
 img = pygame.image.load('SIM.png')
 start_img = pygame.image.load('start.png').convert_alpha()
 exit_img = pygame.image.load('exit.png').convert_alpha()
-start_button = button.Button(250, 200, start_img, 1)
-exit_button = button.Button(250, 300, exit_img, 1)
+start_button = button.BTN(250, 200, start_img, 1)
+exit_button = button.BTN(250, 300, exit_img, 1)
 pygame.display.set_caption('SIM')
 running = True
 
