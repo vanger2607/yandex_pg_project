677c1835e1ee790368e1744f3d7e5dec0216673a
vange <vanger2607@gmail.com>
2021-12-20 13:01:22 +0300

    bullets without reload

--- enemies_and_hero.py
+++ enemies_and_hero.py
@@ -6,19 +6,21 @@ move_to = 'right'
 class Bullet(pg.sprite.Sprite):
     # В разработке
     def __init__(self, img, damage, x, y):
-        pg.sprite.Sprite.__init__(self)
+        super().__init__()
 
-        self.icon = pg.image.load(img)
-        self.rect = self.icon.get_rect()
+        self.image = pg.image.load(img)
+        self.rect = self.image.get_rect()
         self.damage = damage
         self.rect.x = x
         self.rect.y = y
+        self.where = move_to
 
     def update(self, *args):
-        if args[0] == 'left':
+        if self.where == 'left':
             self.rect.x -= 1
-        elif args[0] == 'right':
+        elif self.where == 'right':
             self.rect.x += 1
+
         if self.rect.x <= 10 or self.rect.x >= width - 10:
             self.kill()
 
@@ -30,7 +32,7 @@ class Hero(pg.sprite.Sprite):
     """класс для главного героя нашей игры"""
 
     def __init__(self, img, speed, hp, attack, x, y):
-        pg.sprite.Sprite.__init__(self)
+        super().__init__()
         self.icon = pg.image.load(img)
         self.speed = speed
         self.hp = hp
@@ -44,9 +46,7 @@ class Hero(pg.sprite.Sprite):
         global bullets
         bullet = Bullet('bullet.png', 1, self.rect.centerx, self.rect.top)
         bullets.add(bullet)
-        for i in bullets:
-            i.update(move_to)
-            screen.blit(i.icon, i.rect)
+        bullet.update(move_to)
 
     def update(self, *args):
         if args[0] == 'right' and self.rect.x + self.speed < width - list(self.rect)[2]:
@@ -63,12 +63,12 @@ class Enemy(pg.sprite.Sprite):
     """общий класс для всех врагов"""
 
     def __init__(self, img, speed, hp, attack, x, y):
-        pg.sprite.Sprite.__init__(self)
-        self.icon = pg.image.load(img)
+        super().__init__()
+        self.image = pg.image.load(img)
         self.speed = speed
         self.hp = hp
         self.attack = attack
-        self.rect = self.icon.get_rect()
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
@@ -76,7 +76,9 @@ class Enemy(pg.sprite.Sprite):
         pass
 
     def update(self, *args):
-        pg.time.delay(20)
+
+        if self.hp <= 0:
+            self.kill()
         if args[0][0] > self.rect.x:
             self.rect.x += self.speed
         if args[0][0] < self.rect.x:
@@ -86,11 +88,15 @@ class Enemy(pg.sprite.Sprite):
         if args[0][1] < self.rect.y:
             self.rect.y -= self.speed
 
+    def dead(self, damage):
+        self.hp -= damage
+
 
 pg.init()
 f = pg.font.Font(None, 100)
-
-enemy = Enemy('testenemy.png', 5, 20, 1, 20, 100)
+enemies = pg.sprite.Group()
+enemies.add(Enemy('testenemy.png', 5, 20, 1, 20, 100))
+enemies.add(Enemy('testenemy.png', 5, 20, 1, 50, 60))
 size = width, height = 800, 600
 pg.display.set_caption('way to freedom')
 screen = pg.display.set_mode(size)
@@ -101,7 +107,9 @@ clock = pg.time.Clock()
 bullet = Bullet('bullet.png', 5, 30, 40)
 clock.tick(60)
 while running:
-
+    screen.fill((0, 0, 0))
+    bullets.update()
+    bullets.draw(screen)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -117,8 +125,10 @@ while running:
     if keys[pg.K_DOWN]:
         hero.update('down')
 
-    if pg.sprite.collide_rect(enemy, hero):
+    if pg.sprite.spritecollide(hero, enemies, False):
         hero.hp -= 1
+    for hit in pg.sprite.groupcollide(enemies, bullets, False, True):
+        hit.dead(5)
     if hero.hp <= 0:
         w = f.render('GAME OVER', True,
                      pg.Color('red'))
@@ -126,13 +136,20 @@ while running:
         pg.display.update()
         pg.time.delay(1000)
         running = False
-    enemy.update(hero.rect)
-    screen.fill((0, 0, 0))
+    if len(enemies) == 0:
+        w = f.render('YOU WON', True,
+                     pg.Color('green'))
+        screen.blit(w, (100, 100))
+        pg.display.update()
+        pg.time.delay(1000)
+        running = False
+    pg.time.delay(20)
+    enemies.update(hero.rect)
+
     screen.blit(hero.icon, hero.rect)
-    screen.blit(enemy.icon, enemy.rect)
+    enemies.draw(screen)
 
     if keys[pg.K_SPACE]:
         # В разработке
         hero.attacking('cubes')
-
     pg.display.update()
