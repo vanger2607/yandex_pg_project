e72fa0202106e4a51e57a0fdbd74ef8dd8680dcc
Saveliy125 <Saveliy.korovin@yandex.ru>
2021-12-26 22:10:00 +0300

    Main code fix

--- SIM_Game/main.py
+++ SIM_Game/main.py
@@ -1,36 +1,69 @@
 import pygame
+import pygame_gui
+from pygame import *
 
 pygame.init()
-
-
-class BTN(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
-        super().__init__()
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-
-
 width = 600
 height = 480
 size = (width, height)
-screen = pygame.display.set_mode(size)
+virtual_surface = Surface(size)
+manager = pygame_gui.UIManager((600, 480))
+screen = pygame.display.set_mode(size, RESIZABLE)
+current_size = screen.get_size()
 img = pygame.image.load('SIM.png')
-start_button = BTN(250, 200, 'start.png')
-exit_button = BTN(250, 300, 'exit.png')
+img = transform.scale(img, (600, 480))
 pygame.display.set_caption('SIM')
+switch_start = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((120, 10), (150, 100)),
+    text='START',
+    manager=manager)
+switch_exit = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((120, 110), (150, 100)),
+    text='EXIT',
+    manager=manager)
+switch_Settings = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((120, 210), (150, 100)),
+    text='SETTINGS',
+    manager=manager)
+difficulty = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(
+    options_list=['Easy', 'Medium', 'Hard', 'Very Hard'], starting_option='Easy',
+    relative_rect=pygame.Rect((120, 320), (150, 100)),
+    manager=manager)
 running = True
 screen.blit(img, (0, 0))
-screen.blit(start_button.image, start_button.rect)
-screen.blit(exit_button.image, exit_button.rect)
+clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
+        time_delta = clock.tick(60) / 1000.0
         if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if start_button.rect.collidepoint(event.pos):
-                print('START')
-            elif exit_button.rect.collidepoint(event.pos):
-                print('EXIT')
+            information_dialog = pygame_gui.windows.UIConfirmationDialog(
+                rect=pygame.Rect((250, 200), (300, 200)),
+                manager=manager,
+                window_title='Подтверждение',
+                action_long_desc='Are you seriously?',
+                action_short_name='Yes',
+                blocking=True)
+        elif event.type == VIDEORESIZE:
+            current_size = event.size
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
+                running = False
+            else:
+                screen.blit(img, (0, 0))
+            if event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+                print('difficulty:', event.text)
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == switch_start:
+                    print('START')
+                if event.ui_element == switch_exit:
+                    print('EXIT')
+                if event.ui_element == switch_Settings:
+                    print('SETTINGS')
+        manager.process_events(event)
+        manager.update(time_delta)
+    virtual_surface.blit(img, (0, 0))
+    scaled_surface = transform.scale(virtual_surface, current_size)
+    screen.blit(scaled_surface, (0, 0))
+    manager.draw_ui(screen)
     pygame.display.update()
 pygame.quit()
