f307669e5781b37f158bbba70d5d377f4434046f
Saveliy125 <Saveliy.korovin@yandex.ru>
2022-01-02 21:37:42 +0300

    Main code

--- SIM_Game/main.py
+++ SIM_Game/main.py
@@ -1,20 +1,21 @@
 import pygame
 import pygame_gui
 from pygame import *
+from pygame_gui.elements import UIDropDownMenu
 
 pygame.init()
 width = 600
 height = 480
 size = (width, height)
 virtual_surface = Surface(size)
-manager = pygame_gui.UIManager((600, 480))
+manager = pygame_gui.UIManager((1920, 1080))
 screen = pygame.display.set_mode(size, RESIZABLE)
 current_size = screen.get_size()
 img = pygame.image.load('SIM.png')
 img = transform.scale(img, (600, 480))
 pygame.display.set_caption('SIM')
 pygame.mixer.music.load('Main_sound.mp3')
-pygame.mixer.music.set_volume(0.8)
+pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
 switch_start = pygame_gui.elements.UIButton(
     relative_rect=pygame.Rect((120, 10), (150, 100)),
@@ -38,6 +39,9 @@ clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
         time_delta = clock.tick(60) / 1000.0
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_F11:
+                print('sadasd')
         if event.type == pygame.QUIT:
             information_dialog = pygame_gui.windows.UIConfirmationDialog(
                 rect=pygame.Rect((250, 200), (300, 200)),
@@ -54,14 +58,24 @@ while running:
             else:
                 screen.blit(img, (0, 0))
             if event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
-                print('difficulty:', event.text)
+                if event.ui_element == difficulty:
+                    print('difficulty:', event.text)
+                if event.ui_element == volume:
+                    pygame.mixer.music.set_volume(float(event.text))
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == switch_start:
                     print('START')
+                    pygame.mixer.music.stop()
+                    pygame.display.set_caption('SIM')
+                    pygame.mixer.music.load('level_sound.mp3')
+                    pygame.mixer.music.set_volume(0.8)
+                    pygame.mixer.music.play(-1)
                     size = (width, height)
-                    virtual_surface = Surface(size)
                     manager = pygame_gui.UIManager((1980, 1080))
                     screen = pygame.display.set_mode(size, RESIZABLE)
+                    img = pygame.image.load('fon.png')
+                    img = transform.scale(img, (700, 480))
+                    screen.blit(img, (0, 0))
                     first_level = pygame_gui.elements.UIButton(
                         relative_rect=pygame.Rect((10, 10), (120, 100)),
                         text='1',
@@ -82,16 +96,21 @@ while running:
                         relative_rect=pygame.Rect((490, 10), (110, 100)),
                         text='5',
                         manager=manager)
-                    if event.ui_element == first_level:
-                        print('asa')
-                    if event.ui_element == second_level:
-                        print('asa')
-                    if event.ui_element == third_level:
-                        print('asa')
-                    if event.ui_element == fourth_level:
-                        print('asa')
-                    if event.ui_element == fifth_level:
-                        print('asa')
+                # noinspection PyUnboundLocalVariable
+                if event.ui_element == first_level:
+                    print('Please waiting, 1 level are loading')
+                # noinspection PyUnboundLocalVariable
+                if event.ui_element == second_level:
+                    print('Please waiting, 2 level are loading')
+                # noinspection PyUnboundLocalVariable
+                if event.ui_element == third_level:
+                    print('Please waiting, 3 level are loading')
+                # noinspection PyUnboundLocalVariable
+                if event.ui_element == fourth_level:
+                    print('Please waiting, 4 level are loading')
+                # noinspection PyUnboundLocalVariable
+                if event.ui_element == fifth_level:
+                    print('Please waiting, 5 level are loading')
                 if event.ui_element == switch_exit:
                     print('EXIT')
                     information_dialog = pygame_gui.windows.UIConfirmationDialog(
@@ -103,6 +122,21 @@ while running:
                         blocking=True)
                 if event.ui_element == switch_Settings:
                     print('SETTINGS')
+                    pygame.display.set_caption('SIM')
+                    pygame.mixer.music.load('Main_sound.mp3')
+                    pygame.mixer.music.set_volume(0.2)
+                    pygame.mixer.music.play(-1)
+                    size = (width, height)
+                    manager = pygame_gui.UIManager((1980, 1080))
+                    screen = pygame.display.set_mode(size, RESIZABLE)
+                    img = pygame.image.load('fon.png')
+                    img = transform.scale(img, (700, 480))
+                    screen.blit(img, (0, 0))
+                    volume: UIDropDownMenu = pygame_gui.elements.ui_drop_down_menu.UIDropDownMenu(
+                        options_list=['0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1'],
+                        starting_option='Change volume',
+                        relative_rect=pygame.Rect((300, 200), (250, 100)),
+                        manager=manager)
         manager.process_events(event)
         manager.update(time_delta)
     virtual_surface.blit(img, (0, 0))
