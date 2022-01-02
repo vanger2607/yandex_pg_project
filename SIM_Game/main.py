9827fd022f249e095d8757ad5029f2479d9b27a3
Saveliy125 <Saveliy.korovin@yandex.ru>
2022-01-02 18:45:51 +0300

    Main code add

--- SIM_Game/main.py
+++ SIM_Game/main.py
@@ -13,6 +13,9 @@ current_size = screen.get_size()
 img = pygame.image.load('SIM.png')
 img = transform.scale(img, (600, 480))
 pygame.display.set_caption('SIM')
+pygame.mixer.music.load('Main_sound.mp3')
+pygame.mixer.music.set_volume(0.8)
+pygame.mixer.music.play(-1)
 switch_start = pygame_gui.elements.UIButton(
     relative_rect=pygame.Rect((120, 10), (150, 100)),
     text='START',
@@ -55,8 +58,49 @@ while running:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == switch_start:
                     print('START')
+                    size = (width, height)
+                    virtual_surface = Surface(size)
+                    manager = pygame_gui.UIManager((1980, 1080))
+                    screen = pygame.display.set_mode(size, RESIZABLE)
+                    first_level = pygame_gui.elements.UIButton(
+                        relative_rect=pygame.Rect((10, 10), (120, 100)),
+                        text='1',
+                        manager=manager)
+                    second_level = pygame_gui.elements.UIButton(
+                        relative_rect=pygame.Rect((130, 10), (120, 100)),
+                        text='2',
+                        manager=manager)
+                    third_level = pygame_gui.elements.UIButton(
+                        relative_rect=pygame.Rect((250, 10), (120, 100)),
+                        text='3',
+                        manager=manager)
+                    fourth_level = pygame_gui.elements.UIButton(
+                        relative_rect=pygame.Rect((370, 10), (120, 100)),
+                        text='4',
+                        manager=manager)
+                    fifth_level = pygame_gui.elements.UIButton(
+                        relative_rect=pygame.Rect((490, 10), (110, 100)),
+                        text='5',
+                        manager=manager)
+                    if event.ui_element == first_level:
+                        print('asa')
+                    if event.ui_element == second_level:
+                        print('asa')
+                    if event.ui_element == third_level:
+                        print('asa')
+                    if event.ui_element == fourth_level:
+                        print('asa')
+                    if event.ui_element == fifth_level:
+                        print('asa')
                 if event.ui_element == switch_exit:
                     print('EXIT')
+                    information_dialog = pygame_gui.windows.UIConfirmationDialog(
+                        rect=pygame.Rect((250, 200), (300, 200)),
+                        manager=manager,
+                        window_title='Подтверждение',
+                        action_long_desc='Are you seriously?',
+                        action_short_name='Yes',
+                        blocking=True)
                 if event.ui_element == switch_Settings:
                     print('SETTINGS')
         manager.process_events(event)
