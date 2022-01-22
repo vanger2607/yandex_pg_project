bb50cf7162cdc4ae7801354851b090ac66a67f64
vanger2607 <93196827+vanger2607@users.noreply.github.com>
2022-01-22 23:15:09 +0300

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import time as t
+import shutil
+from typing import Union
+import sys
+import pygame
+import pygame_gui
+from pygame import *
+from pygame_gui._constants import UI_CONFIRMATION_DIALOG_CONFIRMED, UI_BUTTON_PRESSED, OldType
+from pygame_gui.core.interfaces import IUIManagerInterface
+from pygame_gui.elements import UIButton, UITextBox
+from pygame_gui.core import ObjectID
+from random import randint
+import warnings
+from classes_for_game import *
